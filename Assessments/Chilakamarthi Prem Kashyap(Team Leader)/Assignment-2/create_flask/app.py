4e0ea003a5f9b6c9ae1d08af220ec580ba004099
Prem Kashyap Chilakamarthi <72033020+premkashyap004@users.noreply.github.com>
2022-10-10 19:23:16 +0530

    Delete Assessments directory

--- Assessments/Chilakamarthi Prem Kashyap(Team Leader)/Assignment-2/create_flask/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, This is Prem. <br> Welcome to Flask. <br>This is a Flask program!</h1>"
